3ca2168f74430da534347aa7af7d3cdd4991b330
VidyullathaKandipati <latha522@gmail.com>
2017-04-01 14:42:12 +1100

    First commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/deepulatha/Desktop/Desktop/Latha/GA/wdi-19/Projects/djangogirls/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
